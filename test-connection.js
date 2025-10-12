1d241934066064a059388a3aeed16ffb845e4879
nikolajkuzminskij-ux <nikolaj.kuzminskij@gmail.com>
2025-10-12 19:27:14 +0800

    Update test-connection.js

--- test-connection.js
+++ test-connection.js
@@ -2,8 +2,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  'https://abc123def456.supabase.co', // ваш URL
-  'eyJhbGciOiJIUzI1NiIs...' // ваш anon key
+  'https://supabase.com/dashboard/project/tnrbdbtasqaisartsemy/editor/17412?schema=public', // ваш URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucmJkYnRhc3FhaXNhcnRzZW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMDg1NDUsImV4cCI6MjA3NTc4NDU0NX0.YSOeDbp8kj0mG3GMj3wa24DcnwygNBbftMAWl5n8ERA' // ваш anon key
 );
 
 async function testConnection() {
